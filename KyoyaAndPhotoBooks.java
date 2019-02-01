f34e97d807c0243294520fce562cb0b36433ec87
namburuharshith <40354035+namburuharshith@users.noreply.github.com>
2019-02-01 23:45:48 +0530

    Add files via upload

---
+++ KyoyaAndPhotoBooks.java
@@ -0,0 +1,16 @@
+package dailychallenges;
+
+import java.util.*;
+
+public class KyoyaAndPhotoBooks {
+
+	public static void main(String[]args) {
+
+		Scanner sc = new Scanner(System.in);
+	    String n = sc.next();
+	    int l = n.length()+1;
+	    System.out.println((26*(l)-(l-1)));
+	    sc.close();
+	}
+	
+}
